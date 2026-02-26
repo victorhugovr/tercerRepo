8cf0449291815ae8e8f7881f92aa803a6c4a47bd
Victor Vaca <victorhvr.net@gmail.com>
2026-02-25 19:10:51 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                  # Nombre del paquete
+    version="0.1.0",                                    # Versión inicial
+    packages=find_packages(),                           # Paquetes a incluir
+    description="Un paquete pip simple de saludo",      # Breve descripción
+    author="Victor",                                    # Tu nombre
+    author_email="victorhvr.net@gmail.com",             # Tu correo electrónico
+    url="https://github.com/victorhugovr/tercerrepo",   # URL del proyecto
+)
